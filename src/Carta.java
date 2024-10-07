5e68623fd7ddbda4abbbe9d254935bcb9a8bbc75
JesusM15 <guerrajesusm72@gmail.com>
2024-10-07 13:13:08 -0700

    carta basica

--- src/Carta.java
+++ src/Carta.java
@@ -1,8 +1,43 @@
 public class Carta {
     private int valor;
+    private String color;
+    private boolean estaVolteada = false;
 
-    public Carta(int valor) {
+    public Carta(int valor, String color) {
         this.valor = valor;
     }
+
+    public int getValor() {
+        return this.valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isEstaVolteada() {
+        return this.estaVolteada;
+    }
+
+    public void setEstaVolteada(boolean estaVolteada) {
+        this.estaVolteada = estaVolteada;
+    }
+
+    public boolean puedeColocarse(Carta cartaArriba){
+        return (
+                this.valor == cartaArriba.getValor() ||
+                this.color == cartaArriba.getColor() || this.valor == -1
+        );
+    }
+
+
 }
 
