a2f8a0fc1c1fec2c6d44b73c1c39cab22c084ef2
JesusM15 <guerrajesusm72@gmail.com>
2024-10-08 19:58:31 -0700

    comit

--- src/Jugador.java
+++ src/Jugador.java
@@ -4,11 +4,19 @@ public class Jugador {
     private String nombre;
     private int id;
     private Mano mano;
+
+    public Jugador(String nombre, int id) {
+        this.nombre = nombre;
+        this.id = id;
+        mano = new Mano();
+    }
+
     public String getNombre() {return nombre;}
     public void setNombre(String nombre) {this.nombre = nombre;}
     public int getId() {return id;}
     public void setId(int id) {this.id = id;}
     public Mano getMano() {return mano;}
+
     public void tomarCartas(ArrayList<Carta> cartas) {
         mano.recibirCartas(cartas);
     }
--- src/Uno.java
+++ src/Uno.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Uno {
+    private ArrayList<Jugador> jugadores;
+    private int currentTurn = 0;
+
+    public Uno(ArrayList<Jugador> jugadores) {
+        
+    }
 }
